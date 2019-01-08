01d89c8da55d16edff41d75989a38232ae8e4d7c
Lionel Rowe <lionel.rowe@gmail.com>
2019-01-08 13:44:35 +0000

    add loading spinner animation

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import { withNamespaces } from 'react-i18next';
+import { withStyles } from '@material-ui/core';
+
+const styles = theme => ({
+  container: {
+    position: 'relative',
+    width: 120,
+    height: 120,
+    margin: '50px auto'
+  },
+  outer: {
+    animation: '1s spin infinite linear'
+  },
+  inner: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  '@keyframes spin': {
+    from: {
+      transform: 'rotate(0deg)'
+    },
+    to: {
+      transform: 'rotate(360deg)'
+    }
+  }
+});
 
 const Spinner = props => {
+
+  const { t, classes } = props;
+
   return (
-    <div>Loading...</div>
+    <div className={classes.container}>
+      <img
+        src='images/fidget_outer.png'
+        title={t('ui.loading')}
+        alt={t('ui.loading')}
+        className={classes.outer}
+      />
+      <img
+        src='images/fidget_inner.png'
+        alt=''
+        className={classes.inner}
+      />
+    </div>
   );
 }
 
-export default Spinner;
+export default withNamespaces('translations')(withStyles(styles)(Spinner));
