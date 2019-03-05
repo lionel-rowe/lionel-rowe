39e4fa764167d5c768acb00d95a2fcc5b819ae56
Lionel Rowe <lionel.rowe@gmail.com>
2019-03-05 15:18:37 +0800

    fix redirect loop

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,13 @@
 const ghpages = require('gh-pages');
 
 const opts = {
-  branch: 'master',
-  repo: 'https://github.com/lionel-rowe/lionel-rowe.github.io'
+  branch: 'gh-pages',
+  repo: 'https://github.com/lionel-rowe/portfolio/'
 };
 
 const callback = () => {
   console.log(`Published to \`${opts.branch}\` branch at \`${opts.repo}\`.`);
 }
 
+console.log('Publishing...');
 ghpages.publish('build', opts, callback);
--- src/Title.js
+++ src/Title.js
@@ -2,11 +2,11 @@ import React, { useEffect } from 'react';
 import { withNamespaces } from 'react-i18next';
 
 const Title = props => {
-  const { t, i18nKey, children } = props;
+  const { t, i18nKey, lng, children } = props;
 
   useEffect(() => {
     document.title = `${t('ui.title')}${t('global.titleDelimiter')}${t(i18nKey)}`;
-  });
+  }, [ lng, i18nKey ]);
 
   return <React.Fragment children={children} />;
 }
